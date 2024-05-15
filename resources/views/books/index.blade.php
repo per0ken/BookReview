c0e5df126acb39c56760f9e4013d54108ae82438
Lukács Péter <peter.lukacs16@gmail.com>
2024-05-15 14:49:11 +0200

    Fixed books title

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mb-10 text-2x1">Books</h1>
+    <h1 class="mb-10 text-2xl">Books</h1>
 
     <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
         <input type="text" name="title" placeholder="Search by title"
