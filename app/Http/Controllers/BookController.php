9114ed58bf22bf2c8692a772a449e891d0e041ba
Lukács Péter <peter.lukacs16@gmail.com>
2024-05-15 04:12:52 +0200

    Created view and logic for popular and highest rated book filters.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,12 +13,22 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');
 
         $books = Book::when(
             $title,
             fn($query, $title) =>$query->title($title)
-        )
-            ->get();
+        );
+
+        $books = match($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6month' => $books->popularLast6Month(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6month' => $books->highestRatedLast6Month(),
+            default => $books->latest()
+        };
+
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
